2bc6f4aaa7018045653bfdf58277859cd7d87c69
Kainat Khan <kk4710070@gmail.com>
2023-08-08 16:38:43 +0500

    firs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-To-Do-App/"
+  base: "/REACT_TO_DO_APP/"
 })
